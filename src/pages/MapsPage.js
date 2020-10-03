046dfc610a29966ac29992076329a4a67bbdc620
Filipe Benevenuti <filipe.benevenuti@farmaciassaojoao.com.br>
2020-10-03 00:54:23 -0300

    Add evento para apontar a localização no mapa.

--- src/pages/MapsPage.js
+++ src/pages/MapsPage.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  Alert,
+  PermissionsAndroid,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import MapView, {Marker} from 'react-native-maps';
+import Geolocation from 'react-native-geolocation-service';
+import {askPerms} from './AskPerms';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-
 Icon.loadFont();
 
 export default class MapsPage extends React.Component {
@@ -17,6 +25,47 @@ export default class MapsPage extends React.Component {
         longitudeDelta: 0.02,
       },
     };
+  }
+
+  componentDidMount() {
+    if (Platform.OS === 'ios') {
+      Geolocation.requestAuthorization('always');
+    } else {
+      askPerms(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION);
+    }
+  }
+
+  setCurrentPosition() {
+    let getPosition = true;
+
+    if (Platform.OS === 'android') {
+      if (PermissionsAndroid.RESULTS.GRANTED === 'granted') {
+        getPosition = true;
+      } else {
+        getPosition = false;
+        Alert.alert('Permissão de localização não concedida');
+      }
+    }
+
+    if (getPosition) {
+      Geolocation.getCurrentPosition(
+        (position) => {
+          const {latitude, longitude} = position.coords;
+          this.setState({
+            position: {
+              latitude: latitude,
+              longitude: longitude,
+              latitudeDelta: 0.1,
+              longitudeDelta: 0.02,
+            },
+          });
+        },
+        (error) => {
+          console.error(error);
+        },
+        {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+      );
+    }
   }
 
   render() {
@@ -42,7 +91,11 @@ export default class MapsPage extends React.Component {
           </View>
         </View>
 
-        <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            this.setCurrentPosition();
+          }}>
           <Icon name="my-location" color={'#fff'} size={30} />
         </TouchableOpacity>
       </View>
@@ -59,14 +112,14 @@ const styles = StyleSheet.create({
     width: '100%',
   },
   box: {
-    backgroundColor: '#e74c3c',
+    backgroundColor: '#021c3c',
     borderRadius: 20,
     opacity: 0.75,
     marginTop: -170,
     marginHorizontal: 40,
     padding: 25,
     shadowColor: '#000',
-    elevation: 5,
+    //elevation: 5,
   },
   boxTitle: {
     textAlign: 'center',
@@ -79,15 +132,16 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
   },
   button: {
-    backgroundColor: '#e74c3c',
+    backgroundColor: '#e7eaaa',
     borderRadius: 150,
     marginTop: -25,
+    opacity: 0.75,
     width: 50,
     height: 50,
     alignSelf: 'center',
     justifyContent: 'center',
     alignItems: 'center',
     shadowColor: '#000',
-    elevation: 8,
+    //elevation: 8,
   },
 });
